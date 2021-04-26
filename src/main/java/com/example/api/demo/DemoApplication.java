65a16cadb28e6029b5075622aed7dcb4cceea203
chaimaehanabi <77066713+chaimaehanabi@users.noreply.github.com>
2021-04-26 17:37:05 +0200

    commit fichier de test

--- src/main/java/com/example/api/demo/DemoApplication.java
+++ src/main/java/com/example/api/demo/DemoApplication.java
@@ -1,13 +1,28 @@
 package com.example.api.demo;
 
+import com.example.api.demo.Models.Utilisateur;
+import com.example.api.demo.Repository.UtilisateurRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
+    @Autowired
+    private UtilisateurRepository entityManager;
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        entityManager.save(new Utilisateur("chaimae", "hanabi", "cahimae@gmail.com"));
+        entityManager.save(new Utilisateur("safae", "tazi", "safae@gmail.com"));
+        entityManager.save(new Utilisateur("sara", "barrada", "safae@gmail.com"));
+        entityManager.findAll().forEach(c -> {
+            System.out.println(c.toString());
+        });
+    }
 }
